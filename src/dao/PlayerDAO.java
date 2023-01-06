47b4fbc74a2ae2604ff2c57ae05aff4657d88f8f
lej0hn <johnydaras@gmail.com>
2023-01-06 12:02:52 +0200

    Created PlayerDAO interface and class with functions

--- src/dao/PlayerDAO.java
+++ src/dao/PlayerDAO.java
@@ -2,5 +2,10 @@ package dao;
 
 public interface PlayerDAO
 {
+    public void updatePlayer();
+    public void deletePlayer();
+    public void addPlayer();
+
+
 
 }
---
+++ src/dao/PlayerDAOImpl.java
@@ -0,0 +1,13 @@
+package dao;
+
+public class PlayerDAOImpl implements PlayerDAO {
+    public void updatePlayer(){
+
+    }
+    public void deletePlayer(){
+
+    }
+    public void addPlayer(){
+
+    }
+}
