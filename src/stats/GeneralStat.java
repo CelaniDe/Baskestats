72d8dc290e97d99b27041258cf397def990ce417
billys <spongebill98@gmail.com>
2023-01-06 12:39:05 +0200

    Create Generalstat class with all attributes setters and getters

--- src/stats/GeneralStat.java
+++ src/stats/GeneralStat.java
@@ -3,4 +3,39 @@ package stats;
 public class GeneralStat
 {
 
+    private int wins;
+    private int GP;
+    private int Championships;
+
+    public GeneralStat(){}
+
+    public GeneralStat(int wins, int GP, int championships) {
+        this.wins = wins;
+        this.GP = GP;
+        Championships = championships;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getGP() {
+        return GP;
+    }
+
+    public void setGP(int GP) {
+        this.GP = GP;
+    }
+
+    public int getChampionships() {
+        return Championships;
+    }
+
+    public void setChampionships(int championships) {
+        Championships = championships;
+    }
 }
