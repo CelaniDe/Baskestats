71125033b3126e23edde0d3adbb29e27374bb378
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-01-06 12:53:05 +0200

    Created the class Highlights in the package stats

--- src/stats/Highlights.java
+++ src/stats/Highlights.java
@@ -4,14 +4,14 @@ import model.Team;
 
 public class Highlights {
     public Player[] getTop5Scorers(){
-
+        return null;
     }
 
     public Team[] getTop5WinningTeams(){
-
+        return null;
     }
 
     public Player[] getTop5Assisters(){
-
+        return null;
     }
 }
