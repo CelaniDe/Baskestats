7249cd27d3ae792dfd973588b06f28c18d80d24c
Billy <celanide@gmail.com>
2023-01-06 12:28:15 +0200

    Create Welcome page

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,14 @@
 package main;
 
+import ui.Welcome;
+
+import javax.swing.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("Hello world!2");
+        JFrame frame = new Welcome();
     }
 }
---
+++ src/ui/Welcome.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javax.swing.*;
+
+public class Welcome extends JFrame
+{
+    public Welcome()
+    {
+        setTitle("Welcome");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(420,420);
+        setVisible(true);
+    }
+}
