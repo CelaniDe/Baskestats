b46893c542c256daa067ca39dc8702aa228be245
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-01-06 11:38:56 +0200

    Created the class Transfer

---
+++ src/model/Transfer.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Transfer {
+    private String date;
+
+    public Transfer(){}
+
+    public Transfer(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
