adc8ffc4108ad7e937a5ad5c45875206721600b5
KostasKyriakosBtaios <konstantinos.mpatsios@gmail.com>
2023-01-07 12:25:44 +0200

    Created the class Highlights.java in UI

---
+++ src/ui/Highlights.java
@@ -0,0 +1,39 @@
+package ui;
+
+import javax.swing.*;
+
+public class Highlights extends JFrame {
+    private JPanel highlightsPanel;
+    private JLabel scorersLabel;
+    private JLabel firstscorerimg;
+    private JLabel winningTeamsLabel;
+    private JLabel firstTeamImg;
+    private JLabel AssistsLabel;
+    private JLabel firstAssistImg;
+    private JLabel secondAssistImg;
+    private JLabel thirdAssistImg;
+    private JLabel fourthAssistImg;
+    private JLabel fifthAssistImg;
+    private JLabel sixthAssistImg;
+    private JLabel seventhAssistImg;
+    private JLabel eigthAssistImg;
+    private JLabel ninthAssistImg;
+    private JLabel tenthAssistImg;
+    private JLabel secondscorerimg;
+    private JLabel thirdscorerimg;
+    private JLabel fourthscorerimg;
+    private JLabel fifthscorerimg;
+    private JLabel secondTeamImg;
+    private JLabel thirdTeamImg;
+    private JLabel fourthTeamImg;
+    private JLabel fifthTeamImg;
+
+    public Highlights() {
+        setContentPane(highlightsPanel);
+        setTitle("Highlights");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(500,500);
+        setVisible(true);
+    }
+}
