fcc1b75db965bbc5ec71f8a1ceb2b937f8ac4e18
billys <spongebill98@gmail.com>
2023-01-07 12:23:32 +0200

    Create MATCH VS form

---
+++ src/ui/MatchVS.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javax.swing.*;
+
+public class MatchVS extends JFrame {
+
+    private JPanel panel3;
+
+    public MatchVS()
+    {
+        setContentPane(panel3);
+        setTitle("MATCHVS");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(500,500);
+        setVisible(true);
+    }
+}
