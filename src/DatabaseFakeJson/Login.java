0f508c33a1833b57b1b1ac74e60a1250a9e0b40d
Billy <celanide@gmail.com>
2023-01-10 12:26:33 +0200

    Create login method

--- src/DatabaseFakeJson/Login.java
+++ src/DatabaseFakeJson/Login.java
@@ -6,14 +6,12 @@ public class Login
 {
     public Account login(String username, String password) throws WrongCredentialsException
     {
-        if(username == "antonis" && password == "112233!!")
-        {
-            return new Account("antonis","112233!!",0,false);
-        }
-        else if (username == "makis" && password == "112233!!")
-        {
-            return new Account("makis","112233!!",0,true);
-        }
-        throw new WrongCredentialsException();
+        AccountCRUD accountCRUD = new AccountCRUD();
+        Account loggedIn = accountCRUD.getAccount(username,password);
+        if(loggedIn == null)
+            throw new WrongCredentialsException();
+        else
+            return loggedIn;
+
     }
 }
