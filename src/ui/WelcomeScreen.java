f262ed72ca5dbb9289756ecfd95287b130afa0b9
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-11 21:05:33 +0200

    a

--- src/ui/WelcomeScreen.java
+++ src/ui/WelcomeScreen.java
@@ -1,4 +1,26 @@
 package ui;
 
-public class WelcomeScreen {
+import javax.swing.*;
+
+public class WelcomeScreen extends JFrame{
+    private JPanel Master;
+    private JPanel ImgPanel;
+    private JPanel LoginCreatePanel;
+    private JPanel LoginPanel;
+    private JPanel CreatePanel;
+    private JButton ImgButton;
+    private JButton LoginButton;
+    private JLabel LoginPrompt;
+    private JLabel WelcomePrompt;
+    private JButton CreateAccountButton;
+    private JLabel CreatePrompt;
+
+    public WelcomeScreen() {
+        setContentPane(Master);
+        setTitle("Welcome");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(420, 420);
+        setVisible(true);
+    }
 }
