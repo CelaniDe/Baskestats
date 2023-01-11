e82ffbe656afa0d784413bc0dbc78308a11b45c5
Billy <celanide@gmail.com>
2023-01-11 18:56:02 +0200

    Add actions to CRUDNEWS.java UI

--- src/ui/CRUDNEWS.java
+++ src/ui/CRUDNEWS.java
@@ -1,6 +1,14 @@
 package ui;
 
+import dao.NewsDAO;
+import dao.NewsDAOImpl;
+import model.NewsModel;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CRUDNEWS extends JFrame {
     private JTextField newsTitleTextField;
@@ -15,5 +23,14 @@ public class CRUDNEWS extends JFrame {
         setResizable(false);
         setSize(650,650);
         setVisible(true);
+        CREATEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NewsDAO newsDAO = new NewsDAOImpl();
+                List<NewsModel> lista =  new ArrayList<>();
+                lista.add(new NewsModel(newsTitleTextField.getText(),descriptionTextArea.getText()));
+                newsDAO.addNews(lista);
+            }
+        });
     }
 }
