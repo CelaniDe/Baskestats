285e1d54002f1f76392a5591f4ff626c880892a7
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-12 19:33:58 +0200

    Διαφορα

--- src/ui/Match.java
+++ src/ui/Match.java
@@ -4,6 +4,18 @@ import javax.swing.*;
 
 public class Match extends  JFrame {
     private JPanel Master;
+    private JPanel Navigation;
+    private JButton NavMatch;
+    private JButton NavLeague;
+    private JButton NavSearch;
+    private JButton NavNews;
+    private JButton NavHighlights;
+    private JLabel MatchesTitle;
+    private JPanel Header;
+    private JPanel Mid;
+    private JButton PastButton;
+    private JButton PresentButton;
+    private JButton FutureButton;
 
     public Match()
     {
