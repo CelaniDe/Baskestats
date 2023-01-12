ee906cec2507fd1f4424df1cbae61057c4b6a968
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-12 13:56:31 +0200

    Δημιουργια WelcomeScreen

---
+++ src/ui/TeamProfile.java
@@ -0,0 +1,33 @@
+package ui;
+
+import javax.swing.*;
+
+public class TeamProfile extends JFrame {
+    private JPanel Master;
+    private JPanel Content;
+    private JPanel Navigation;
+    private JButton NavMatch;
+    private JButton NavLeague;
+    private JButton NavSearch;
+    private JButton NavNews;
+    private JButton NavHighlights;
+    private JLabel TeamProfileTitle;
+    private JPanel Header;
+    private JButton TeamLogoButton;
+    private JLabel TeamName;
+    private JPanel Mid;
+    private JLabel TeamSlogan;
+    private JLabel GamesPlayed;
+    private JLabel Wins;
+    private JTable TeamPlayersTable;
+    private JScrollPane PlayerTablePane;
+
+    public TeamProfile() {
+        setContentPane(Master);
+        setTitle("Welcome");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(1000, 1000);
+        setVisible(true);
+    }
+}
