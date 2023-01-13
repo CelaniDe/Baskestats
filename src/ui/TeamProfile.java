f660fdecfed5c884d285ef0392d68cf973b1651d
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2023-01-13 13:09:12 +0200

    UI changes

--- src/ui/TeamProfile.java
+++ src/ui/TeamProfile.java
@@ -5,22 +5,24 @@ import javax.swing.*;
 public class TeamProfile extends JFrame {
     private JPanel Master;
     private JPanel Content;
+    private JPanel Header;
+    private JPanel Mid;
+    private JLabel GamesPlayed;
+    private JLabel Wins;
+    private JTable TeamPlayersTable;
+    private JScrollPane PlayerTablePane;
+    private JPanel ImgPanel;
+    private JButton TeamImgButton;
+    private JPanel TeamTitle;
+    private JLabel TeamTitleLabel;
+    private JLabel TeamNameLabel;
+    private JLabel TeamSloganLabel;
     private JPanel Navigation;
     private JButton NavMatch;
     private JButton NavLeague;
     private JButton NavSearch;
     private JButton NavNews;
     private JButton NavHighlights;
-    private JLabel TeamProfileTitle;
-    private JPanel Header;
-    private JButton TeamLogoButton;
-    private JLabel TeamName;
-    private JPanel Mid;
-    private JLabel TeamSlogan;
-    private JLabel GamesPlayed;
-    private JLabel Wins;
-    private JTable TeamPlayersTable;
-    private JScrollPane PlayerTablePane;
 
     public TeamProfile() {
         setContentPane(Master);
