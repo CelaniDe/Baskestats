9052a8b38a89cafb01e0c7fce1bfbda4365c89ba
Billy <celanide@gmail.com>
2023-01-20 11:34:38 +0200

    Adjust Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,13 +17,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //System.out.println("Hello world!2");
-        JFrame frame1 = new Highlights(new GeneralStatsDAOImpl(), new PlayerDAOImpl());
-
-        GeneralStatsCRUD generalStatsCRUD = new GeneralStatsCRUD();
-        for(GeneralStats generalStats : generalStatsCRUD.getTop3GeneralStatsByWinsForTeams())
-        {
-            System.out.println(generalStats.toStringTeam());
-        }
+        JFrame frame1 = new WelcomeScreen();
     }
 }
